1f487a8943db71108e8288b51f00fc42e9a76c47
Kattar Hindu <161199195+KattarHindu-in@users.noreply.github.com>
2024-03-13 10:28:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Krishnauff/8xRocky",
+    "https://github.com/KattarHindu-in/Kattar-Hindu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
